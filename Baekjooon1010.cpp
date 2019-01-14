4e5247011ff6154cbe3bc2b28b40adfbc4fd48c0
CheeseB <2489ckckck@naver.com>
2019-01-14 23:19:38 +0900

    modify

--- Baekjooon1010.cpp
+++ Baekjooon1010.cpp
@@ -3,7 +3,22 @@
 using namespace std;
 
 int main(){
+	
+	int test, i;
+	
+	cin >> test;
+	
+	int *n = new int[test];
+	int *m = new int[test];
+	
+	for(i=0; i<test; i++)
+		cin >> n[i] >> m[i];
+		
 	//code
+	
+		
+	delete[] n;
+	delete[] m;
 	
 	return 0;
 }
